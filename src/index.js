3f6944e0f2f3f5e8d24cf11bdfe1a4e72f3e7036
sameer <sameersaini40@gmail.com>
2018-08-24 16:49:33 -0600

    added youtube-api-search and YOUTUBE_API_KEY

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const YOUTUBE_API_KEY = 'AIzaSyBWv_U6vfEdx02ZzZguTtECzP5A4rv6LWI';
 
 // create a new component which contains HTML
 const App = () => (<div> Hello world</div>);

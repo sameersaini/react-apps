9abbd1193e51fb91973c1b1d44429a8a3912fc42
sameer <sameersaini40@gmail.com>
2018-08-24 16:41:37 -0600

    added empty files for youtube app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom';
 
 
 // create a new component which contains HTML
-const App = () => {
-    return (
-        <div>
-            Hello world
-        </div>
-    );
-};
+const App = () => (<div> Hello world</div>);
+
 
 // generate the html and put it onto the page.
 ReactDOM.render(<App />, document.getElementById('root'));
